5732356b4e57f92c5080e0920a8467a4c951f9e9
jihongju <daniel.jihong.ju@gmail.com>
2017-05-19 13:43:18 +0200

    Add output translate to vgg

--- cifar/models.py
+++ cifar/models.py
@@ -1,7 +1,7 @@
 """CNN model."""
 from __future__ import print_function
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 
@@ -34,7 +34,7 @@ def CNN(input_shape, num_classes):
 
     return model
 
-def VGG8(input_shape, num_classes):
+def VGG8(input_shape, num_classes, t_vec=None):
 
     model = Sequential()
 
@@ -48,7 +48,7 @@ def VGG8(input_shape, num_classes):
 
     model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Conv2D(64, 3, 3))
+    model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(LeakyReLU(alpha=0.2))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.3))
@@ -66,7 +66,13 @@ def VGG8(input_shape, num_classes):
     model.add(Dropout(0.5))
 
     model.add(Dense(num_classes))
+    # output translate (tensorflow only)
+    if t_vec is not None:
+        def translate(x):
+            return x - t_vec
+        model.add(Lambda(lambda x: translate(x)))
     model.add(Activation('softmax'))
 
     return model
+
 
