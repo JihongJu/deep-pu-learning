bf9e4eb2cb46e53489ab765360c9e91f26ce3dfc
jihongju <daniel.jihong.ju@gmail.com>
2017-06-22 14:27:23 -0500

    LeakyRELU and he_normal

--- experiments/cifar/models.py
+++ experiments/cifar/models.py
@@ -38,34 +38,34 @@ def VGG8(input_shape, num_classes, t_vec=None):
 
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3), padding='same',
+    model.add(Conv2D(32, (3, 3), padding='same', kernel_initializer='he_normal',
                                 input_shape=input_shape))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(Conv2D(32, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(LeakyReLU(alpha=0.2))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(LeakyReLU(alpha=0.2))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.3))
 
-    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(Conv2D(128, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(Conv2D(128, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(LeakyReLU(alpha=0.2))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
 
     model.add(Flatten())
-    model.add(Dense(512))
+    model.add(Dense(512, kernel_initializer='he_normal'))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
-    model.add(Dense(num_classes))
+    model.add(Dense(num_classes, kernel_initializer='he_normal'))
     # output translate (tensorflow only)
     if t_vec is not None:
         def translate(x):
