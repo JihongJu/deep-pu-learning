46e334f49a008469d6d8ec52e9e3add1c18a0152
jihongju <daniel.jihong.ju@gmail.com>
2017-06-23 05:36:36 -0500

    Add fade in to the unlabeled exponential loss

--- pulearn/losses/keras_losses.py
+++ pulearn/losses/keras_losses.py
@@ -1,11 +1,19 @@
 import keras.backend as K
+from keras.losses import categorical_crossentropy
 from .tensorflow_losses import cross_entropy_and_exponential_loss
 
 
-def unlabelled_exponential_loss(y_true, y_pred):
+def unlabeled_exponential_loss(y_true, y_pred):
     """Class dependent loss.
 
     Using cross entropy for P and absolute difference for U.
     """
     return cross_entropy_and_exponential_loss(probs=y_pred,
                                               labels=y_true)
+
+
+
+def fade_in_unlabeled_exponential_loss(y_true, y_pred):
+    """Controll unlabeled exponential loss fade in with alpha."""
+    return alpha * categorical_crossentropy(y_true, y_pred) \
+            + (1 - alpha) * unlabeled_exponential_loss(y_true, y_pred)
